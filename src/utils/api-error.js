6342c6a66c65baaf8524dd8b49ebdefe8877c444
ASHFAQAKHTAR\hunnn <ashfaq.akhtar@occamsadvisory.com>
2025-04-23 14:28:47 +0530

    Api Response file added

--- src/utils/api-error.js
+++ src/utils/api-error.js
@@ -3,15 +3,22 @@ class ApiError extends Error {
         statusCode,
         message = "",
         errors = [],
-        stack = ""
+        stack = "",
     ) {
         super(message);
         this.statusCode = statusCode;
         this.message = message;
         this.sucess = false;
         this.errors = errors;
-        
 
+
+        if (stack) {
+            this.stack = stack
+        } else {
+
+            Error.captureStackTrace(this, this.constructor)
+
+        }
     }
 }
 
