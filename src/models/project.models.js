b6c2b162ec2d45b78b1470819ca7c82c417774a6
ASHFAQAKHTAR\hunnn <ashfaq.akhtar@occamsadvisory.com>
2025-04-24 13:29:29 +0530

    Model for Project done

--- src/models/project.models.js
+++ src/models/project.models.js
@@ -1,6 +1,22 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema, Types } from "mongoose";
 
-const projectSchema = new Schema({})
+const projectSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true // will remove all space from the username string
+    },
+    description: {
+        type: String
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+}, { timestamps: true });
 
 
 
